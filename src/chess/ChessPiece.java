23506b651f4e8b98e310109ba1983d0a6dab1356
guiihh532 <guiihh532@gmail.com>
2021-04-11 18:10:49 -0300

    Pawn

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,6 +7,7 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece{
 
 	private Color color;
+	private int moveCount;
 	
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -15,6 +16,12 @@ public abstract class ChessPiece extends Piece{
 	
 	public Color getColor() {
 		return color;
+	}
+	public void increaseMoveCount(){
+		moveCount ++;
+	}
+	public void decreaseMoveCount(){
+		moveCount --;
 	}
 	
 	public ChessPosition getChessPosition(){
@@ -26,4 +33,10 @@ public abstract class ChessPiece extends Piece{
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
+
+	public int getMoveCount() {
+		return moveCount;
+	}
+
+
 }
